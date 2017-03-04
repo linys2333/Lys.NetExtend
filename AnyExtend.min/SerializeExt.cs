25a492e37916db205c598ba4571cd4d7b1747260
lyssyl <973368640@qq.com>
2017-03-04 19:41:42 +0800

    AnyExtend.min.dll

---
+++ AnyExtend.min/SerializeExt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace AnyExtend
+{
+    /// <summary>
+    /// 各类序列化方法
+    /// </summary>
+    public static class SerializeExt
+    {
+        #region 对象序列化
+        
+        /// <summary>
+        /// 对象深度复制（对象需标记为可序列化）
+        /// </summary>
+        public static T DeepClone<T>(this object obj) where T : class
+        {
+            using (var ms = new MemoryStream())
+            {
+                var s = new BinaryFormatter();
+                s.Serialize(ms, obj);
+                ms.Position = 0;
+                return s.Deserialize(ms) as T;
+            }
+        }
+
+        #endregion
+    }
+}
