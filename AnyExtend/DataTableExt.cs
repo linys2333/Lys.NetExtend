f9fb74f7b64028050b5f101f6b408e3eee32de29
lyssyl <973368640@qq.com>
2017-04-14 18:53:21 +0800

    添加DataTable扩展

---
+++ AnyExtend/DataTableExt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace AnyExtend
+{
+    public static class DataTableExt
+    {
+        public static void RemoveCol(this DataTable dt, string col)
+        {
+            if (dt.Columns.Contains(col))
+            {
+                dt.Columns.Remove(col);
+            }
+        }
+    }
+}
